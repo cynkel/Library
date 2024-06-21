8caed5c0044179edfca89be270d2eb2129bda0f7
Kelvin <kelxir@outlook.com>
2024-06-21 03:04:33 +0000

    Display book objects on card using JS

--- script.js
+++ script.js
@@ -1,12 +1,49 @@
 const collection = [];
 const newBookButton = document.querySelector(".nbbutton");
 const booksContainer = document.querySelector(".booksContainer");
-const bookCard = document.querySelector(".bookCard");
-const titleDiv = document.querySelector(".title");
-const authorDiv = document.querySelector(".name");
 const noPages = document.querySelector(".nop");
 const modal = document.querySelector(".modal-form");
 const closeButton = document.querySelector(".close");
+
+
+function createNewBook(title, author, pages) {
+    const bookCard = document.createElement("div");
+    bookCard.classList.toggle("bookCard");
+
+    const titleDiv = document.createElement("div");
+    titleDiv.classList.toggle("title");
+    titleDiv.textContent = title;
+    bookCard.appendChild(titleDiv);
+
+    const authorDiv = document.createElement("div");
+    authorDiv.classList.toggle("author");
+    bookCard.appendChild(authorDiv);
+
+    const bySpan = document.createElement("span");
+    bySpan.classList.toggle("by");
+    bySpan.textContent = "By ";
+    authorDiv.appendChild(bySpan);
+
+    const nameSpan = document.createElement("span");
+    nameSpan.classList.toggle("name");
+    nameSpan.textContent = author;
+    authorDiv.appendChild(nameSpan);
+
+    const pagesDiv = document.createElement("div");
+    pagesDiv.classList.toggle("pages");
+    bookCard.appendChild(pagesDiv);
+
+    const numberOfPagesSpan = document.createElement("span");
+    numberOfPagesSpan.classList.toggle("nop");
+    numberOfPagesSpan.textContent = pages + " ";
+    pagesDiv.appendChild(numberOfPagesSpan);
+
+    const spanPages = document.createElement("span");
+    spanPages.textContent = "pages";
+    pagesDiv.appendChild(spanPages);
+
+    booksContainer.appendChild(bookCard);
+}
 
 newBookButton.addEventListener("click", () => modal.style.display = "block");
 closeButton.addEventListener("click", () => modal.style.display = "none");
@@ -28,11 +65,11 @@ function addBookToCollection() {
 
 function showBooks() {
     for (let index = 0; index < collection.length; index++) {
-        const element = collection[index];
-        console.log(element);
+        createNewBook(collection[index].title, collection[index].author, collection[index].pages);
     }
 }
 
+addBookToCollection();
 addBookToCollection();
 addBookToCollection();
 addBookToCollection();
